aced1842af0c526f997b3e26adff4651d7caf6aa
Manish Kumar Pandey <896849@cognizant.com>
2024-02-11 19:51:19 +0530

    created a new smlal react app for date counter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<>
+<Step />
+
+</>
+  );
+}
+
+function Step() {
+
+  const [step, setStep] = useState(1);
+
+  function increase () {
+    return setStep((s) => s + 1); 
+  
+  }
+
+  function decrease () {
+    if (step > 1) {  return setStep((s) => s - 1); }
+  }
+
+
+  return (
+    <>
+    <div className="step">
+      <button onClick={decrease}>-</button>
+      <p>Step : {step}</p>
+      <button onClick={increase}>+</button>
     </div>
+    <Counter step={step}/>
+    </>
+  );
+
+}
+
+function Counter({step}) {
+  const [count, setCount] = useState(0);
+  // setting the date
+  const date = new Date();
+  date.setDate(date.getDate() + count);
+
+  function increase () {
+    return setCount((c) => c + step); 
+  
+  }
+
+  function decrease () {
+    return setCount((c) => c - step); 
+  }
+
+
+
+  return (
+    <>
+    <div className='counter'>
+    <button onClick={
+      decrease}>-</button>
+      <p>Count: {count}</p>
+      <button onClick={increase}>+</button>
+      
+    </div>
+    <p className='date'>{count === 0 ? " Today is " : `${count} days from today is `}{date.toDateString()}</p>
+    </>
   );
 }
 
